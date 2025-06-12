b62b3d29604da34b01195834b91862a6547c1931
kandadave <kandachumo@gmail.com>
2025-06-12 07:58:26 +0300

    array methods

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const messages = []
+
+function writeCards(names,event){
+    for(let name of names){
+        let thanks = `Thank you, ${name}, for the wonderful ${event} gift!`
+        messages.push(thanks)
+    }
+    console.log(messages)
+    return messages;
+}
+
+writeCards();
+
+function countDown(num){
+    for(let i=num;i>=0;i--){
+        console.log(i)
+    }
+}
+
+countDown();
